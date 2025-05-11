81d01d82b5ee32f126b345d540cfc8d35591e03c
samarth-jangda <samarth.jangda@gmail.com>
2025-05-11 15:55:55 +0000

    Pushing code for text description of an image

--- modules/sketch_to_video/inference/image_text.py
+++ modules/sketch_to_video/inference/image_text.py
@@ -3,6 +3,7 @@
 
 import os
 import json
+import math
 import openai
 import argparse
 from PIL import Image
@@ -61,11 +62,7 @@ class ObjectsMetaData:
     def __init__(self,image_id,detr_model_data):
         self.detected_image_id = image_id
         self.detected_data = detr_model_data
-        self.metadata = {
-            image_id:{
-                "labels": detr_model_data[image_id]["image_labels"]
-            }
-        }
+        
 
     def object_count(self):
         """
@@ -73,7 +70,7 @@ class ObjectsMetaData:
         number of objects as detected by object detection model
         """
         total_objects = len(self.detected_data[self.detected_image_id]["object_id"])
-        self.metadata[self.detected_image_id]["objects_count"] = total_objects
+        return total_objects
 
     def object_location(self):
         """
@@ -90,13 +87,26 @@ class ObjectsMetaData:
             mid_y = (line_1[0][1] + line_1[1][1]) / 2
             intersection_point = (mid_x,mid_y)
             intersections.append(intersection_point)
-        self.metadata[self.detected_image_id["intersection_points"]] = intersections
+        return intersections
 
     def object_area(self):
         """
         The following function is used to find the area of rectangle
         formed by the bounding box of object in the sketch image.
+        applying distance formulae to find length and bredth.
+        dictanc_formulate = sqroot( sq(x2-x1) + sq(y2-y1) ) 
         """
+        object_areas=[]
+        for boxes in self.detected_data[self.detected_image_id]["bbox"]:
+            number_1 = boxes[0][0] - boxes[1][0]
+            number_2 = boxes[0][1] - boxes[1][1]
+            length_1 = math.sqrt(math.pow(number_1,2) + math.pow(number_2,2))
+            number_3 = boxes[0][0] - boxes[3][0]
+            number_4 = boxes[0][1] - boxes[3][1]
+            length_2 = math.sqrt(math.pow(number_3,2) + math.pow(number_4,2))
+            object_area = length_1 * length_2
+            object_areas.append(object_area)
+        return object_areas    
 
 class ObjectRelationship:
     """
@@ -181,7 +191,15 @@ def process_data():
         detected_image_data = [detr_data for detr_data in detr_data_file if image_name == detr_data["image_id"].split("-")[1]]
         if detected_image_data !=[]:
             image_id = list(detected_image_data.keys())[0]
-            object_metadata = ObjectsMetaData(image_id,detected_image_data[0])
+            metadata = {
+                image_id:{
+                    "labels": detected_image_data[0][image_id]["image_labels"]
+                }
+            }
+            object_metadata_class = ObjectsMetaData(image_id,detected_image_data[0])
+            metadata[image_id]["object_count"] = object_metadata_class.object_count()
+            metadata[image_id]["objct_locations"] = object_metadata_class.object_location()
+            metadata[image_id]["object_areas"] = object_metadata_class.object_area()
 
 
 if "__main__" == __name__:
